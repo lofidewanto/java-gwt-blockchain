8235b5be6555f56d37f01677eadd05c24192fbd1
lofidewanto <lofidewanto@gmail.com>
2022-02-02 22:35:47 +0100

    First import

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # java-gwt-blockchain
 Java GWT Blockchain Example
+
+mvn archetype:generate -DgroupId=com.lofi.app -DartifactId=java-gwt-blockchain -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+
---
+++ src/main/java/com/lofi/app/client/BlockChainEntryPoint.java
@@ -0,0 +1,41 @@
+package com.lofi.app.client;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class BlockChainEntryPoint implements EntryPoint {
+
+	private static Logger logger = Logger
+			.getLogger(BlockChainEntryPoint.class.getName());
+
+	private static int prefix = 4;
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			logger.info("Hello BlockChain!");
+
+			List<Block> blockchain = new ArrayList<Block>();
+			Block genesisBlock = new Block("The is the Genesis Block.", "0", new Date().getTime());
+        	genesisBlock.mineBlock(prefix);
+        	blockchain.add(genesisBlock);
+        	Block firstBlock = new Block("The is the First Block.", genesisBlock.getHash(), new Date().getTime());
+        	firstBlock.mineBlock(prefix);
+        	blockchain.add(firstBlock);
+
+			Block newBlock = new Block("The is a New Block.", blockchain.get(blockchain.size() - 1)
+                .getHash(), new Date().getTime());
+        	newBlock.mineBlock(prefix);
+        	blockchain.add(newBlock);
+		});
+
+		RootPanel.get("helloButton").add(button);
+	}
+}
